399ec69973e217c8590e7dbda267d7822673bd8a
Sven Azari <94923703+svenazari@users.noreply.github.com>
2022-03-28 09:33:40 +0200

    Add files via upload

---
+++ avg.py
@@ -0,0 +1,266 @@
+#avg.py
+#autor: Sven Azari
+#naredbe: del, izl, show, memload, memclear
+#Kako bi se mogla koristiti naredba memload, u istom folderu kao i skripta mora biti i datoteka '.avg_mem.txt'
+
+import sys
+from os import system, name
+from os.path import exists
+
+def clear (): #čišćenje zaslona
+  if name == 'nt':
+      _ = system('cls')
+  else:
+      _ = system('clear')
+
+def average ():
+    clear ()
+    if exists('.avg_mem.txt') == False: #provjera da li postoji datoteka .avg_mem.txt - ako je nema, skripta ju kreira kako bi bio omogućen upis nakon kalkulacija
+        f = open('.avg_mem.txt', 'w+')
+    Traz = [] #lista za razlike u temperaturi zraka
+    Uraz = [] #lista za razlike u relativnoj vlazi zraka
+    while True:
+        Tk = input ("Tk = ") #temperatura zraka klasično
+        if Tk == "show":
+            print (Traz)
+            print (Uraz)
+            print ("* * * ")
+            continue
+        elif Tk == "del":
+            clear()
+            del Traz[-1]
+            del Uraz[-1]
+            Tsred = str(round(sum(Traz) / len(Traz),1)) #izračun prosječne vrijednosti razlike temperature zraka (len(Traz) - dužina liste)
+            Usred = str(round(sum(Uraz) / len(Uraz))) #izračun prosječne vrijednosti razlike relativne vlage zraka
+            #ispis
+            print ("# dTs = " + Tsred + " #")
+            print ("# dUs = " + Usred + " #")
+            print ("(Srednju razliku treba dodati na podatak AMP-a)")
+            print ("* * * ")
+            continue
+        elif Tk == "memload": #učitavanje spremljenih podataka razlike - memload će izbrisati svaki postojeći izračun
+            clear()
+            Traz.clear()
+            Uraz.clear()
+            meml = []
+            memlf = [] #float od meml
+            with open('.avg_mem.txt') as mem:
+                meml = mem.readlines() #čitanje linija i upis u listu
+            for line in meml: #upis u novu lisu kao float
+                memlx = float(line)
+                memlf.append(memlx)
+            x = int(len(memlf) / 2) #polovina od memlf
+            Tmem = memlf[:x] #učitavanje razlike temperature
+            Umem = memlf[x:] #učitavanje razlike vlage
+            Traz.extend(Tmem) #dodavanje memorije na listu razlike temperature
+            Uraz.extend(Umem) #dodavanje memorije na listu razlike vlage
+            Tsred = str(round(sum(Traz) / len(Traz),1)) #izračun prosječne vrijednosti razlike temperature zraka (len(Traz) - dužina liste)
+            Usred = str(round(sum(Uraz) / len(Uraz))) #izračun prosječne vrijednosti razlike relativne vlage zraka
+            #ispis
+            print ("# dTs = " + Tsred + " #")
+            print ("# dUs = " + Usred + " #")
+            print ("(Srednju razliku treba dodati na podatak AMP-a)")
+            print ("* * * ")
+            continue
+        elif Tk == "memclear": #čišćenje memorije skripte
+            clear()
+            Traz.clear()
+            Uraz.clear()
+            print ("Memorija skripte je očišćena!")
+            continue
+        elif Tk == "izl":
+            clear()
+            exit()
+            continue
+        Ta = input ("Ta = ") #temperatura zraka automatsko
+        if Ta == "show":
+            print (Traz)
+            print (Uraz)
+            continue
+        elif Ta == "del":
+            clear()
+            del Traz[-1]
+            del Uraz[-1]
+            Tsred = str(round(sum(Traz) / len(Traz),1)) #izračun prosječne vrijednosti razlike temperature zraka (len(Traz) - dužina liste)
+            Usred = str(round(sum(Uraz) / len(Uraz))) #izračun prosječne vrijednosti razlike relativne vlage zraka
+            #ispis
+            print ("# dTs = " + Tsred + " #")
+            print ("# dUs = " + Usred + " #")
+            print ("(Srednju razliku treba dodati na podatak AMP-a)")
+            print ("* * * ")
+            continue
+        elif Ta == "memload": #učitavanje spremljenih podataka razlike - memload će izbrisati svaki postojeći izračun
+            clear()
+            Traz.clear()
+            Uraz.clear()
+            meml = []
+            memlf = [] #float od meml
+            with open('.avg_mem.txt') as mem:
+                meml = mem.readlines() #čitanje linija i upis u listu
+            for line in meml: #upis u novu lisu kao float
+                memlx = float(line)
+                memlf.append(memlx)
+            x = int(len(memlf) / 2) #polovina od memlf
+            Tmem = memlf[:x] #učitavanje razlike temperature
+            Umem = memlf[x:] #učitavanje razlike vlage
+            Traz.extend(Tmem) #dodavanje memorije na listu razlike temperature
+            Uraz.extend(Umem) #dodavanje memorije na listu razlike vlage
+            Tsred = str(round(sum(Traz) / len(Traz),1)) #izračun prosječne vrijednosti razlike temperature zraka (len(Traz) - dužina liste)
+            Usred = str(round(sum(Uraz) / len(Uraz))) #izračun prosječne vrijednosti razlike relativne vlage zraka
+            #ispis
+            print ("# dTs = " + Tsred + " #")
+            print ("# dUs = " + Usred + " #")
+            print ("(Srednju razliku treba dodati na podatak AMP-a)")
+            print ("* * * ")
+            continue
+        elif Ta == "memclear": #čišćenje memorije skripte
+            clear()
+            Traz.clear()
+            Uraz.clear()
+            print ("Memorija skripte je očišćena!")
+            continue
+        elif Ta == "izl":
+            clear()
+            exit()
+            continue
+        print ("*")
+        Uk = input ("Uk = ") #vlaga zraka klasično
+        if Uk == "show":
+            print (Traz)
+            print (Uraz)
+            continue
+        elif Uk == "del":
+            clear()
+            del Traz[-1]
+            del Uraz[-1]
+            Tsred = str(round(sum(Traz) / len(Traz),1)) #izračun prosječne vrijednosti razlike temperature zraka (len(Traz) - dužina liste)
+            Usred = str(round(sum(Uraz) / len(Uraz))) #izračun prosječne vrijednosti razlike relativne vlage zraka
+            #ispis
+            print ("# dTs = " + Tsred + " #")
+            print ("# dUs = " + Usred + " #")
+            print ("(Srednju razliku treba dodati na podatak AMP-a)")
+            print ("* * * ")
+            continue
+        elif Uk == "memload": #učitavanje spremljenih podataka razlike - memload će izbrisati svaki postojeći izračun
+            clear()
+            Traz.clear()
+            Uraz.clear()
+            meml = []
+            memlf = [] #float od meml
+            with open('.avg_mem.txt') as mem:
+                meml = mem.readlines() #čitanje linija i upis u listu
+            for line in meml: #upis u novu lisu kao float
+                memlx = float(line)
+                memlf.append(memlx)
+            x = int(len(memlf) / 2) #polovina od memlf
+            Tmem = memlf[:x] #učitavanje razlike temperature
+            Umem = memlf[x:] #učitavanje razlike vlage
+            Traz.extend(Tmem) #dodavanje memorije na listu razlike temperature
+            Uraz.extend(Umem) #dodavanje memorije na listu razlike vlage
+            Tsred = str(round(sum(Traz) / len(Traz),1)) #izračun prosječne vrijednosti razlike temperature zraka (len(Traz) - dužina liste)
+            Usred = str(round(sum(Uraz) / len(Uraz))) #izračun prosječne vrijednosti razlike relativne vlage zraka
+            #ispis
+            print ("# dTs = " + Tsred + " #")
+            print ("# dUs = " + Usred + " #")
+            print ("(Srednju razliku treba dodati na podatak AMP-a)")
+            print ("* * * ")
+            continue
+        elif Uk == "memclear": #čišćenje memorije skripte
+            clear()
+            Traz.clear()
+            Uraz.clear()
+            print ("Memorija skripte je očišćena!")
+            continue
+        elif Uk == "izl":
+            clear()
+            exit()
+            continue
+        Ua = input ("Ua = ") #vlaga zraka automatsko
+        if Ua == "show":
+            print (Traz)
+            print (Uraz)
+            continue
+        elif Ua == "del":
+            clear()
+            del Traz[-1]
+            del Uraz[-1]
+            Tsred = str(round(sum(Traz) / len(Traz),1)) #izračun prosječne vrijednosti razlike temperature zraka (len(Traz) - dužina liste)
+            Usred = str(round(sum(Uraz) / len(Uraz))) #izračun prosječne vrijednosti razlike relativne vlage zraka
+            #ispis
+            print ("# dTs = " + Tsred + " #")
+            print ("# dUs = " + Usred + " #")
+            print ("(Srednju razliku treba dodati na podatak AMP-a)")
+            print ("* * * ")
+            continue
+        elif Ua == "memload": #učitavanje spremljenih podataka razlike - memload će izbrisati svaki postojeći izračun
+            clear()
+            Traz.clear()
+            Uraz.clear()
+            meml = []
+            memlf = [] #float od meml
+            with open('.avg_mem.txt') as mem:
+                meml = mem.readlines() #čitanje linija i upis u listu
+            for line in meml: #upis u novu lisu kao float
+                memlx = float(line)
+                memlf.append(memlx)
+            x = int(len(memlf) / 2) #polovina od memlf
+            Tmem = memlf[:x] #učitavanje razlike temperature
+            Umem = memlf[x:] #učitavanje razlike vlage
+            Traz.extend(Tmem) #dodavanje memorije na listu razlike temperature
+            Uraz.extend(Umem) #dodavanje memorije na listu razlike vlage
+            Tsred = str(round(sum(Traz) / len(Traz),1)) #izračun prosječne vrijednosti razlike temperature zraka (len(Traz) - dužina liste)
+            Usred = str(round(sum(Uraz) / len(Uraz))) #izračun prosječne vrijednosti razlike relativne vlage zraka
+            #ispis
+            print ("# dTs = " + Tsred + " #")
+            print ("# dUs = " + Usred + " #")
+            print ("(Srednju razliku treba dodati na podatak AMP-a)")
+            print ("* * * ")
+            continue
+        elif Ua == "memclear": #čišćenje memorije skripte
+            clear()
+            Traz.clear()
+            Uraz.clear()
+            print ("Memorija skripte je očišćena!")
+            continue
+        elif Ua == "izl":
+            clear()
+            exit()
+            continue
+        clear ()
+        Tkf = float (Tk)
+        Taf = float (Ta)
+        Ukf = float (Uk)
+        Uaf = float (Ua)
+        Tra = round(Tkf - Taf,1)
+        Ura = round(Ukf - Uaf)
+        Traz.append(Tra) #dodavanje razlike na listu temperature zraka
+        Uraz.append(Ura) #dodavanje razlike na listu relativne vlage zraka
+        Tsred = str(round(sum(Traz) / len(Traz),1)) #izračun prosječne vrijednosti razlike temperature zraka (len(Traz) - dužina liste)
+        Usred = str(round(sum(Uraz) / len(Uraz))) #izračun prosječne vrijednosti razlike relativne vlage zraka
+        #ispis
+        print ("# dTs = " + Tsred + " #")
+        print ("# dUs = " + Usred + " #")
+        print ("(Srednju razliku treba dodati na podatak AMP-a)")
+        print ("* * * ") 
+        #spremanje razlike za učitavanje kod novog pokretanja skripte 
+        Trazs = []
+        Urazs = []
+        for line in Traz: #pretvaranje u str
+            lines = str(round(line,1))
+            Trazs.append(lines) #upis str u Trazs
+        for line in Uraz:
+            lines1 = str(round(line))
+            Urazs.append(lines1) #upis str u Urazs
+        upis = open(".avg_mem.txt", "w") #otvaranje datoteke za upis
+        for line in Trazs: #upis razlike temperature
+            upis.write(line)
+            upis.write('\n')
+        for line in Urazs: #upis razlike vlage
+            upis.write(line)
+            upis.write('\n')
+        upis.close() #zatvaranje datoteke
+        if Tk == "izl" or Ta == "izl" or Uk == "izl" or Ua == " izl":
+            break
+    exit ()
+
+average()
